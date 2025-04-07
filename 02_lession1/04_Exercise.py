b3dbe7c04b9b2c5532dc3c2356d5bc8652c70680
SonicAaryan <aaryansonar2@gmail.com>
2025-04-07 18:02:37 +0530

    answer added

--- 02_lession1/04_Exercise.py
+++ 02_lession1/04_Exercise.py
@@ -1,1 +1,13 @@
 # Write a py pg to print the contents of a dir using the os module. search online for the function which does that.
+
+import os
+
+# Specify the directory path
+directory_path = '.'  # Current directory
+
+# List all entries in the specified directory
+entries = os.listdir(directory_path)
+
+# Print each entry
+for entry in entries:
+    print(entry)
