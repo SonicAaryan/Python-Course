f4a55dac9a1e0910aa58a81a59fa6b30822b0237
SonicAaryan <aaryansonar2@gmail.com>
2025-04-07 17:00:49 +0530

    environment setup guide and basic code

---
+++ 01_Modules_Comments_PIP/01firstProgram.py
@@ -0,0 +1,6 @@
+print("Hello World")
+ 
+# MODULES : A module is a file containing code written by somebody else which can be imported and used in our programs.
+# TYPES OF MODULES : 1) Built in Modules(preinstalled in python) -> E.g os,random etc.    2) External Modules (Need to install using pip)-> E.g tensorflow,flask, etc.
+
+# PIP : Pip is the package manager for python . You can use pip to install a module on your system.
