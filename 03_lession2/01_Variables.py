bbf8a50fffdbd607f4d0a1d0aeb53fb13592f9a9
SonicAaryan <aaryansonar2@gmail.com>
2025-04-08 11:35:09 +0530

    chapter 3 added

---
+++ 03_lession2/01_Variables.py
@@ -0,0 +1,20 @@
+# Primarily these are the following data types in Python
+# 1) Integers 2) Floating point numbers 2) Strings 4) Boolean 5) None
+
+a = 1 # integer
+b = 4
+print('Total =',a+b)
+
+GSTValue = 2700.50 # floating point number 
+
+name = 'ABC' # string
+print("value inside variable is :",name)
+
+isAdmin = True # boolean 
+print(isAdmin) 
+
+currentValue = None # none type
+print(currentValue)
+print(type(currentValue)) # type of value
+ 
+#Further you can practice on variable , arithmetic operations , comparison operators , logical operations .
---
+++ 03_lession2/02_TypeCast.py
@@ -0,0 +1,7 @@
+age = "19" # string data 
+print(str(age)) # type casting
+print(type(int(age)))
+print(type(age))
+
+userName = input("Enter Your Name :")
+print("Your name is :",userName)
