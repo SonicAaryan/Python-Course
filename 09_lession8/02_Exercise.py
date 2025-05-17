ebcfd9c26494a13395980b1d5cfe37333d06e3bf
SonicAaryan <aaryansonar2@gmail.com>
2025-05-17 18:32:25 +0530

    09_lession added

--- 09_lession8/02_Exercise.py
+++ 09_lession8/02_Exercise.py
@@ -1,1 +1,7 @@
 # Q. Write a recursive function to calculate the sum of first n natural numbers.
+def sumOfNum(num):
+    if(num==1):
+        return 1
+    return sumOfNum(num-1)+num
+
+print("Sum of Natural No are :",sumOfNum(5))
--- 09_lession8/03_Exercise.py
+++ 09_lession8/03_Exercise.py
@@ -1,1 +1,13 @@
 # Q. Write a python function to remove a given word form a list & strip it at the same time.
+
+def remove(list,word):
+    n = []
+    for item in list:
+        if not(item == word):
+            n.append(item.strip(word))
+    return n
+            
+list = ["anHarry","Rohan","Joy","ryan"] 
+print(remove(list,"an"))
+
+# strip(word) removes an from <front and the end> of the word
