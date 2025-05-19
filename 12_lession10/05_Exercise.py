53d67b628e80cfda59a7ca05a11fa33f79311e89
SonicAaryan <aaryansonar2@gmail.com>
2025-05-19 12:26:09 +0530

    10_lession completed

--- 12_lession10/05_Exercise.py
+++ 12_lession10/05_Exercise.py
@@ -1,1 +1,38 @@
-# asd
+# Write a class Train which has methods to book a ticket,get status(no of seats) and get fare info. of train running under indian railways.
+
+class Train:
+    ticketFare = 325
+    noOfSeats = 100
+    travelRoute = "Neral to Matheran"     
+
+    def bookTicket(self, noOfTicket):
+        if noOfTicket <= self.noOfSeats: 
+            self.noOfSeats -= noOfTicket  # Update available seats
+            print(f"The Number of Train Tickets you have booked is: {noOfTicket}\n")
+        else:
+            print(f"Only {self.noOfSeats} seats are available. You requested {noOfTicket}.\n")
+    
+    def getStatus(self):
+        print(f"Currently the train has -> {self.noOfSeats} <- seats left! BOOK NOW :)\n")
+
+    def trainInfo(self):
+        print(f"The train ticket fare is -> ₹{self.ticketFare} <- and the route is: {self.travelRoute}")
+
+customer1 = Train()
+
+# Book Ticket
+bookTicket = int(input("How many train tickets do you want to book?"))
+customer1.bookTicket(bookTicket)
+
+# Seat Status Check
+seatStatus = input("Do you want to check the seat status?\nType 'YES' = ")
+if seatStatus.strip().lower() == "yes": 
+    customer1.getStatus()
+
+# Train Info Check
+trainInfo = input("Do you want to check the train route and fare?\nType 'YES' = ")
+if trainInfo.strip().lower() == "yes": 
+    customer1.trainInfo()
+
+        
+        
---
+++ 12_lession10/06_Exercise.py
@@ -0,0 +1,11 @@
+# Can you change the self-parameter inside a class to something else(say "omen").Try changing self to "slf" or "omen" and see the effects.
+
+class Valorant:
+    def __init__(omen):
+        print("Object Created")
+
+game1 = Valorant()
+
+'''
+Yes, your code is correct — and it works because in Python, the self parameter is not a keyword, just a convention.(It's a standard to use)
+'''
