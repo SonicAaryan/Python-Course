93b90fedfe7a4338b3d3095973864033b241102b
SonicAaryan <aaryansonar2@gmail.com>
2025-08-02 16:52:38 +0530

    file updated

--- 09_lession8/02_Fruntion_with_argument.py
+++ 09_lession8/02_Fruntion_with_argument.py
@@ -1,7 +1,7 @@
 def goodDay(name, ending):
-    print("Good Day "+ name)
+    print("Good Day "+ name , end="")
     print(ending)
 
-goodDay("Aaryan","Thank you")
-goodDay("Prax","Thank you")
-goodDay("Aru","Arigato gozaimasu")
+goodDay("Aaryan ","Thank you")
+goodDay("Prax ","Thank you")
+goodDay("Aru ","")
